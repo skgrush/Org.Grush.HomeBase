5188cc55aedea688a5e9ac0cdb54650b28768f04
Samuel K. Grush <Samuel@Grush.org>
2026-06-04 11:06:45 -0500

    WIP init and lib

---
+++ Org.Grush.HomeBase.WeatherStationLib/WeatherStationClient.cs
@@ -0,0 +1,141 @@
+using System.Collections.Immutable;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using FluentModbus;
+
+namespace Org.Grush.HomeBase.WeatherStationLib;
+
+public readonly record struct WindSpeedAndDirection(
+  double WindSpeed,
+  ushort WindDirection,
+  ushort WindDirectionAngle
+);
+
+public readonly record struct AllData(
+  float WindSpeed,
+  ushort _regF5,
+  ushort WindDirection,
+  ushort WindDirectionAngle,
+  float RelativeHumidity,
+  float Temperature,
+  float NoiseDb,
+  ushort Pm2_5,
+  ushort Pm10,
+  ushort AtmosphericPressure,
+  UInt32 Lux
+);
+
+
+
+/// <summary>
+///
+///
+/// </summary>
+/// <remarks>
+///   Addresses we know:
+///     0x01_F4 : WindSpeed in 0.01m/s
+///       ....
+///     0x01_F6 : WindDirection (?)
+///     0x01_F7 : WindDirectionAngle in degrees
+///     0x01_F8 : RelativeHumidity in 0.1%
+///     0x01_F9 : Temperature in 0.1ºC
+///     0x01_FA : Noise in 0.1 dB
+///       ....
+///     0x01_FE : Lux MS2B
+///     0x01_FF : Lux LS2B
+/// </remarks>
+public sealed class WeatherStationClient : IAsyncDisposable
+{
+  private readonly CancellationTokenSource cts = new();
+
+  private readonly ModbusRtuClient rtuClient = new()
+  {
+    Parity = Parity.None,
+    StopBits = StopBits.One,
+    Handshake = Handshake.None,
+  };
+  private readonly string _port;
+  private readonly byte _modbusUnitIdentifier;
+
+  public const int DefaultBaud = 4800;
+  public static readonly ImmutableDictionary<byte, int> SupportedBauds =
+  [
+    new(0, 2400),
+    new(1, 4800),
+    new(2, 9600),
+    new(3, 19200),
+    new(4, 38400),
+    new(5, 57600),
+    new(6, 115200),
+    new(7, 1200),
+  ];
+
+  public WeatherStationClient(
+    string port,
+    byte modbusUnitIdentifier,
+    int? baudRate
+  )
+  {
+    _port = port;
+    _modbusUnitIdentifier = modbusUnitIdentifier;
+
+    if (baudRate is not null && !SupportedBauds.ContainsValue(modbusUnitIdentifier))
+      throw new ArgumentOutOfRangeException(nameof(baudRate), baudRate, $"baudRate must be one of {string.Join(',', SupportedBauds)}");
+
+    rtuClient.BaudRate = baudRate ?? DefaultBaud;
+    rtuClient.Connect(_port, ModbusEndianness.BigEndian);
+  }
+
+  public bool Cancelled { get; private set; }
+
+  public async Task<AllData> ReadAllDataAsync()
+  {
+    var result = await rtuClient.ReadHoldingRegistersAsync<ushort>(
+      unitIdentifier: _modbusUnitIdentifier,
+      startingAddress: 0x01_F0,
+      count: 16
+    );
+
+    return new(
+      WindSpeed: result.Span[0x4] / 100.0f,
+      _regF5: result.Span[0x5],
+      WindDirection: result.Span[0x6],
+      WindDirectionAngle: result.Span[0x7],
+      RelativeHumidity: result.Span[0x8] / 10.0f,
+      Temperature: result.Span[0x9] / 10.0f,
+      NoiseDb: result.Span[0xA] / 10.0f,
+      Pm2_5: result.Span[0xB],
+      Pm10: result.Span[0xC],
+      AtmosphericPressure: result.Span[0xD],
+      Lux: MemoryMarshal.Cast<ushort, UInt32>(result.Span[0xE..])[0]
+    );
+  }
+
+  public async Task<WindSpeedAndDirection> ReadWindSpeedAndDirectionAsync(string port, byte unitIdentifier)
+  {
+
+    rtuClient.Connect(port, ModbusEndianness.BigEndian);
+
+    var result = await rtuClient.ReadHoldingRegistersAsync<ushort>(
+      unitIdentifier: unitIdentifier,
+      startingAddress: 0x01_F4,
+      count: 4
+    );
+
+    return new(
+      WindSpeed: result.Span[0] / 100.0d,
+      WindDirection: result.Span[2],
+      WindDirectionAngle: result.Span[3]
+    );
+  }
+
+  public async ValueTask DisposeAsync()
+  {
+    if (Cancelled)
+      return;
+
+    await cts.CancelAsync();
+    cts.Dispose();
+    rtuClient.Dispose();
+  }
+}
