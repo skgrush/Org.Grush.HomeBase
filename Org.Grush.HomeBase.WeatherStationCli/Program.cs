14b6dd2c977156932628b4fa0692f9d9c0d00a08
Samuel K. Grush <Samuel@Grush.org>
2026-06-05 13:19:05 -0500

    SetAction

--- Org.Grush.HomeBase.WeatherStationCli/Program.cs
+++ Org.Grush.HomeBase.WeatherStationCli/Program.cs
@@ -31,90 +31,97 @@ Option<byte?> loopOption = new("--loop")
   Arity = ArgumentArity.ZeroOrOne,
 };
 HelpOption helpOption = new();
-
 RootCommand command = new("HomeBase WeatherStationCli")
 {
   busIdOption,
   baudRateOption,
   helpOption,
 };
+command.SetAction(Run);
 
 Console.Error.WriteLine($"Args: {string.Join(' ', args)}");
 ParseResult parseResult = command.Parse(args);
 var invoked = await parseResult.InvokeAsync();
 Console.Error.WriteLine($"Invoked: {invoked}");
 
-if (parseResult.GetResult(helpOption) is not null)
-  return await parseResult.InvokeAsync();
+return invoked;
 
-if (parseResult.Errors.Count is not 0)
+// if (parseResult.GetResult(helpOption) is not null)
+//   return await parseResult.InvokeAsync();
+//
+// if (parseResult.Errors.Count is not 0)
+// {
+//   foreach (var error in parseResult.Errors)
+//     Console.WriteLine(error);
+//   return 1;
+// }
+
+async Task<int> Run(ParseResult parseResult)
 {
-  foreach (var error in parseResult.Errors)
-    Console.WriteLine(error);
-  return 1;
-}
 
+  using Board board = Board.Create();
 
-
-using Board board = Board.Create();
-
-try
-{
-  var componentInformation = board.QueryComponentInformation();
-  BoardPrinter.PrintComponentInfo(componentInformation, "");
-}
-catch {}
-
-if (board is RaspberryPiBoard piBoard)
-{
-  if (!piBoard.IsSpiActivated())
+  try
   {
-    Console.Error.WriteLine("SPI is not activated");
-    return 2;
+    var componentInformation = board.QueryComponentInformation();
+    BoardPrinter.PrintComponentInfo(componentInformation, "");
   }
-}
-
-using SpiDevice spiDevice = board.CreateSpiDevice(new(
-  busId: parseResult.GetRequiredValue(busIdOption),
-  chipSelectLine: parseResult.GetValue(chipSelectLineOption) ?? -1
-));
-using ModbusRtuSpiPort rtuSpiPort = new(spiDevice);
-
-await using WeatherStationClient client = new(
-  rtuSpiPort,
-  1,
-  parseResult.GetValue(baudRateOption)
-);
-
-byte? loopTimeout =
-  parseResult.GetResult(loopOption) is null
-    ? null
-    : (parseResult.GetValue(loopOption) ?? 5)
-;
-
-CancellationTokenSource cts = new();
-Console.CancelKeyPress += (_, _) => cts.Cancel();
-
-try
-{
-  Console.WriteLine("{\n");
-  while (!cts.Token.IsCancellationRequested && !client.Cancelled)
+  catch
   {
-    var results = await client.ReadAllDataAsync();
-    Console.WriteLine(
-      "\"{0}\": {1},\n",
-      DateTimeOffset.Now,
-      JsonSerializer.Serialize(results, WeatherStationLibSerializerContext.Default.AllData)
-    );
-
-    if (loopTimeout is null)
-      break;
-    await Task.Delay(loopTimeout.Value, cts.Token);
   }
-}
-catch (OperationCanceledException)
-{
-}
-Console.WriteLine("}");
 
-return 0;
+  if (board is RaspberryPiBoard piBoard)
+  {
+    if (!piBoard.IsSpiActivated())
+    {
+      Console.Error.WriteLine("SPI is not activated");
+      return 2;
+    }
+  }
+
+  using SpiDevice spiDevice = board.CreateSpiDevice(new(
+    busId: parseResult.GetRequiredValue(busIdOption),
+    chipSelectLine: parseResult.GetValue(chipSelectLineOption) ?? -1
+  ));
+  using ModbusRtuSpiPort rtuSpiPort = new(spiDevice);
+
+  await using WeatherStationClient client = new(
+    rtuSpiPort,
+    1,
+    parseResult.GetValue(baudRateOption)
+  );
+
+  byte? loopTimeout =
+      parseResult.GetResult(loopOption) is null
+        ? null
+        : (parseResult.GetValue(loopOption) ?? 5)
+    ;
+
+  CancellationTokenSource cts = new();
+  Console.CancelKeyPress += (_, _) => cts.Cancel();
+
+  try
+  {
+    Console.WriteLine("{\n");
+    while (!cts.Token.IsCancellationRequested && !client.Cancelled)
+    {
+      var results = await client.ReadAllDataAsync();
+      Console.WriteLine(
+        "\"{0}\": {1},\n",
+        DateTimeOffset.Now,
+        JsonSerializer.Serialize(results, WeatherStationLibSerializerContext.Default.AllData)
+      );
+
+      if (loopTimeout is null)
+        break;
+      await Task.Delay(loopTimeout.Value, cts.Token);
+    }
+  }
+  catch (OperationCanceledException)
+  {
+  }
+
+  Console.WriteLine("}");
+
+  return 0;
+}
