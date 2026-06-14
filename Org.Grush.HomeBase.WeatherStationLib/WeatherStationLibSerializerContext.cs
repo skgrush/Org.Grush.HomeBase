3d892315c1cabda99f93ee775fb0c7f1b98824ef
Samuel K. Grush <Samuel@Grush.org>
2026-06-13 22:37:00 -0500

    improve

--- Org.Grush.HomeBase.WeatherStationLib/WeatherStationLibSerializerContext.cs
+++ Org.Grush.HomeBase.WeatherStationLib/WeatherStationLibSerializerContext.cs
@@ -2,7 +2,6 @@ using System.Text.Json.Serialization;
 
 namespace Org.Grush.HomeBase.WeatherStationLib;
 
-[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSourceGenerationOptions(WriteIndented = true, UseStringEnumConverter = true)]
 [JsonSerializable(typeof(AllData))]
-[JsonSerializable(typeof(WindSpeedAndDirection))]
 public partial class WeatherStationLibSerializerContext : JsonSerializerContext;
