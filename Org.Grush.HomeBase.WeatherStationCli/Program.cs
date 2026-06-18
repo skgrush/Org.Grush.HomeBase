adc6a46fc9b0402bc01425638e63e54ecd68a97b
Samuel K. Grush <Samuel@Grush.org>
2026-06-17 22:19:42 -0500

    RootCommandAction tidy

--- Org.Grush.HomeBase.WeatherStationCli/Program.cs
+++ Org.Grush.HomeBase.WeatherStationCli/Program.cs
@@ -2,15 +2,31 @@
 
 using System.CommandLine;
 using Iot.Device.Common;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Org.Grush.HomeBase.APRSWXNET;
 using Org.Grush.HomeBase.WeatherStationCli;
 
-await using Stream stdoutStream = Console.OpenStandardOutput();
 Console.SetOut(Console.Error);
-using SimpleConsoleLoggerFactory loggerFactory = new();
+
+ServiceCollection services = [];
+services
+  .AddKeyedTransient<Stream>(serviceKey: "stdout", implementationFactory: (_, _) => Console.OpenStandardOutput())
+  .AddLogging()
+  .AddSingleton<SimpleConsoleLoggerFactory>()
+  .AddSingleton<ILoggerFactory>(sp => sp.GetRequiredService<SimpleConsoleLoggerFactory>())
+  .AddSingleton<RootCommandAction>()
+  .AddScoped<RootCommandAction.Executor>()
+  .AddAprsWxNetServices()
+  .AddStorageDb(o => o.WithDbFile("Org.Grush.HomeBase.WeatherStationCli"))
+;
+
+await using var serviceProvider = services.BuildServiceProvider();
+
 
 RootCommand command = new("HomeBase WeatherStationCli");
 CliOptionResult.AddOptions(command.Options);
-command.Action = new CliExecutor(stdoutStream, loggerFactory);
+command.Action = serviceProvider.GetRequiredService<RootCommandAction>();
 
 InvocationConfiguration invocationConfiguration = new()
 {
@@ -20,8 +36,9 @@ InvocationConfiguration invocationConfiguration = new()
 Console.Error.WriteLine($"Args: {string.Join(' ', args)}");
 
 ParseResult parseResult = command.Parse(args);
+
 if (parseResult.GetValue(CliOptionResult.LogLevelOption) is { } logLevel)
-  loggerFactory.MinLogLevel = logLevel;
+  serviceProvider.GetRequiredService<SimpleConsoleLoggerFactory>().MinLogLevel = logLevel;
 
 int invoked = await parseResult.InvokeAsync(invocationConfiguration);
 Console.Error.WriteLine($"Invoked: {invoked}");
---
+++ Org.Grush.HomeBase.WeatherStationCli/RootCommandAction.cs
@@ -0,0 +1,100 @@
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Device.Gpio;
+using System.IO.Ports;
+using System.Text.Json;
+using FluentModbus;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Org.Grush.HomeBase.APRSWXNET;
+using Org.Grush.HomeBase.WeatherStationLib.SEN0658;
+
+namespace Org.Grush.HomeBase.WeatherStationCli;
+
+public class RootCommandAction(
+  IServiceScopeFactory scopeFactory
+) : AsynchronousCommandLineAction
+{
+  public override async Task<int> InvokeAsync(ParseResult parseResult, CancellationToken cancellationToken = default)
+  {
+    CliOptionResult result = CliOptionResult.From(parseResult);
+
+    await using var scope = scopeFactory.CreateAsyncScope();
+
+    var executor = scope.ServiceProvider.GetRequiredService<Executor>();
+
+    return await executor.ExecuteAsync(result, cancellationToken);
+  }
+
+  public class Executor(
+    [FromKeyedServices("stdout")] Stream stdout,
+    ILoggerFactory loggerFactory,
+    ILogger<RootCommandAction> programLogger
+  )
+  {
+    public async Task<int> ExecuteAsync(CliOptionResult result, CancellationToken cancellationToken)
+    {
+      programLogger.LogDebug("Args: {cliOptionResult}", result);
+
+      // using LibGpiodV2Driver driver = new(4);
+      // using GpioController controller = new(driver);
+
+      using GpioController controller = new();
+
+      programLogger.LogDebug(
+        "GPIO Controller ({type}) pinCount={pinCount}",
+        controller.GetType().Name,
+        controller.PinCount
+      );
+
+      using SerialPort serialPort = new(
+        result.Device,
+        baudRate: result.BaudRate,
+        parity: Parity.None,
+        dataBits: 8,
+        stopBits: StopBits.OnePointFive
+      );
+
+      ModbusRtuSerialPort modbusPort = new(serialPort);
+
+      await using WeatherStationClient client = new(
+        modbusPort,
+        1,
+        loggerFactory.CreateLogger<WeatherStationClient>()
+      );
+
+      Utf8JsonWriter stdoutWriterUtf8 = new(stdout, new()
+      {
+        Indented = true,
+        NewLine = "\n",
+      });
+
+      try
+      {
+        stdoutWriterUtf8.WriteStartObject();
+        await stdoutWriterUtf8.FlushAsync(cancellationToken);
+        while (!cancellationToken.IsCancellationRequested && !client.Cancelled)
+        {
+          var results = await client.ReadAllDataAsync(cancellationToken);
+          var nowStr = DateTimeOffset.Now.ToString("u");
+
+          stdoutWriterUtf8.WritePropertyName(nowStr);
+          JsonSerializer.Serialize(writer: stdoutWriterUtf8, value: results, AprsWxNetSerializerContext.Default.SEN0658AllData);
+          await stdoutWriterUtf8.FlushAsync(cancellationToken);
+          programLogger.LogDebug("Wrote data at ts={ts}", nowStr);
+
+          if (result.Loop is null)
+            break;
+          await Task.Delay(result.Loop.Value, cancellationToken);
+        }
+      }
+      catch (OperationCanceledException)
+      {
+      }
+
+      stdout.Write("\n}\n"u8);
+
+      return 0;
+    }
+  }
+}
